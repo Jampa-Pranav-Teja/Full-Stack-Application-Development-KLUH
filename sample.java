952a1209c1a9acd5208c4069554696fb9ee97c77
Focused-Fool <pranavtejajampa@gmail.com>
2025-12-23 13:26:58 +0530

    added files

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 public class sample {
     public static void main(String[] args) {
         System.out.println("Hello World"); //To print Hello World
+        System.out.println("Hello World again"); //To print Hello World
     }
 }
